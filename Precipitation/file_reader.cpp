f8a2a91de201e06a7137748581084147bd0901fb
Anastasia Aravina <aaquariuss13@gmail.com>
2024-01-16 15:37:14 +0300

    Реализовано чтение данных из файла

---
+++ Precipitation/file_reader.cpp
@@ -0,0 +1,32 @@
+#include "file_reader.h"
+#include "constants.h"
+
+#include <fstream>
+#include <cstring>
+
+
+void read(const char* file_name, precipitation* array[], int& size)
+{
+    std::ifstream file(file_name);
+    if (file.is_open())
+    {
+        size = 0;
+        char tmp_buffer[MAX_STRING_SIZE];
+        while (!file.eof())
+        {
+            precipitation* item = new precipitation;
+            file >> item->day;
+            file >> item->month;
+            file >> item->amount;
+            file >> tmp_buffer;
+            file.read(tmp_buffer, 1); // чтения лишнего символа пробела
+            file.getline(item->property, MAX_STRING_SIZE);
+            array[size++] = item;
+        }
+        file.close();
+    }
+    else
+    {
+        throw "Ошибка открытия файла";
+    }
+}
---
+++ Precipitation/file_reader.h
@@ -0,0 +1,8 @@
+#ifndef FILE_READER_H
+#define FILE_READER_H
+
+#include "precipitation.h"
+
+void read(const char* file_name, precipitation* array[], int& size);
+
+#endif
