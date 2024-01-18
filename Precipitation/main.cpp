3f2ec80975213ddfe6bed0f3db5d257919077bdd
Anastasia Aravina <aaquariuss13@gmail.com>
2024-01-18 21:38:50 +0300

    Добавлены тестовые данные

--- Precipitation/main.cpp
+++ Precipitation/main.cpp
@@ -9,3 +9,15 @@ int main()
     cout << "Author: Anastasia Aravina\n";
     return 0;
 }
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #1. GIT\n";
+    cout << "Variant #0. Precipitation\n";
+    cout << "Author: Anastasia Aravina\n";
+    cout << "Group: 23PINJ1z\n";
+    return 0;
+}
